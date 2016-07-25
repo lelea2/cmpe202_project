bd245e0c921f4cac8210de607e9e5da5ab74dd71
Dao <kdao@kdao01sjl.attlocal.net>
2016-07-24 22:23:50 -0700

    Initial check in

--- src/Base/DataHashMap.java
+++ src/Base/DataHashMap.java
@@ -1,4 +1,4 @@
-package DataHashMap;
+package Base;
 
 import java.util.ArrayList;
 import java.util.HashMap;
--- src/Report/Report.java
+++ src/Report/Report.java
@@ -1,7 +1,44 @@
 package Report;
 
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
 /**
  * Created by kdao on 7/22/16.
  */
-public class Report {
+
+public abstract class Report {
+    protected String _description;
+    protected String _title;
+    private String _timestamp;
+
+    /**
+     *  Constructor
+     */
+    public Report() {
+        _timestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date());
+    }
+
+    public abstract void printReport();
+
+    public String get_timestamp() {
+        return _timestamp;
+    }
+
+    public String get_description() {
+        return _description;
+    }
+
+    public void set_description(String description) {
+        _description = description;
+    }
+
+    public String get_title() {
+        return _title;
+    }
+
+    public void set_title(String title) {
+        _title = title;
+    }
+
 }
