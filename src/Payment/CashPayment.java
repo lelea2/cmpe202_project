fe439bbb23af841423e9c30ac9735605bf42b1fc
Dao <kdao@kdao01sjl.attlocal.net>
2016-08-07 16:14:42 -0700

    Initial set up for payment

--- src/Payment/CashPayment.java
+++ src/Payment/CashPayment.java
@@ -3,5 +3,22 @@ package Payment;
 /**
  * Created by kdao on 8/7/16.
  */
-public class CashPayment {
+public class CashPayment extends Payment {
+    public CashPayment(String payerName) {
+        _payerName = payerName ;
+    }
+
+    @Override
+    public void setupPayment(){
+    }
+
+    @Override
+    public void processPayment() {
+        _paymentSucceeded = true ;
+    }
+
+    @Override
+    public void printReceipt() {
+
+    }
 }
--- src/Payment/CreditCardPayment.java
+++ src/Payment/CreditCardPayment.java
@@ -1,7 +1,30 @@
 package Payment;
 
+import java.util.Scanner;
+
 /**
  * Created by kdao on 8/7/16.
  */
-public class CreditCardPayment {
+public class CreditCardPayment extends Payment {
+    private String _ccNumber;
+    private String _ccExpiration;
+
+    public CreditCardPayment(String payerName) {
+        _payerName = payerName ;
+    }
+
+    @Override
+    public void setupPayment() {
+        Scanner scan = new Scanner(System.in);
+        _ccNumber = scan.next();
+        _ccExpiration = scan.next();}
+
+    @Override
+    public void processPayment() {
+        _paymentSucceeded = true ;
+    }
+
+    @Override
+    public void printReceipt() {
+    }
 }
