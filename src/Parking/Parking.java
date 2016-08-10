78138c19c830959a2f2690b2c0dd8b20c86fd7f0
yucheng <chengyu.0302@gmail.com>
2016-08-09 22:01:33 -0700

    initial check in for parking

--- src/Parking/Parking.java
+++ src/Parking/Parking.java
@@ -4,20 +4,47 @@ import java.util.ArrayList;
 
 /**
  * Created by kdao on 8/3/16.
+ * Modified by ycheng on 8/9/16
  */
-public class Parking {
-    int slotNumber;
 
-    public Parking() {
+public class Parking {
+
+    private int totalSlotNumber;
+    private int availableSlotNumber;
+    private boolean[] parkingSlots;
+
+
+    public Parking(int totalSlotNumber) {
+        this.totalSlotNumber = totalSlotNumber;
+        this.parkingSlots = new boolean[totalSlotNumber];
     }
 
     public ArrayList<Integer> getAvailableSlots() {
         ArrayList<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < this.parkingSlots.length; i++){
+            if (!this.parkingSlots[i]){
+                arr.add(i);
+            }
+        }
         return arr;
     }
 
-    public int getSlotAvailable() {
-        return slotNumber;
+    public int getAvailableSlotNumber() {
+        return this.availableSlotNumber;
+    }
+
+    public boolean enterParkingSlot(int slot){
+        if (slot < 0 || slot >= this.totalSlotNumber || this.parkingSlots[slot]){
+            return false;
+        }
+        this.parkingSlots[slot] = true;
+        return true;
+    }
+
+    public void exitParkingSlot(int slot) {
+        if (slot >= 0 && slot < this.totalSlotNumber){
+            this.parkingSlots[slot] = false;
+        }
     }
 
     public void pickSpot(int spotNumber) {
