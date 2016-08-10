189bd3c90e20911689b37cc928193a52a10f8138
yuexingyin <yyfyou1991@gmail.com>
2016-08-09 19:40:06 -0700

    Updated feedback module

---
+++ src/Feeback/ServiceFeedback.java
@@ -0,0 +1,16 @@
+package Feeback;
+
+/**
+ * Created by YuexingYin on 8/9/16.
+ */
+public class ServiceFeedback extends Feedback {
+
+    public ServiceFeedback(String content, String name, int rating) {
+        super(content, name, rating);
+    }
+
+    public void setType(){
+        System.out.println("This is a service feedback.");
+    }
+
+}
